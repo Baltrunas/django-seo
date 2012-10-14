4635a8b06e8b5cb22293d358a6f99ee59a099cb5
Stanislav Baltrunas <stanislav@baltrunas.ru>
2012-10-14 18:14:13 +0600

    Rename model add Tag model

--- admin.py
+++ admin.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*
 from django.contrib import admin
-
 from seo.models import MetaData
+
 
 class MetaDataAdmin(admin.ModelAdmin):
 	list_display = ['title', 'header', 'url', 'public', 'created_at']
 	search_fields = ['title', 'header', 'url', 'public', 'created_at']
 	list_filter = ['public']
 	list_editable = ['public']
+
 	class Media:
 		js = ('tiny_mce/tiny_mce.js', 'tiny_mce/textareas.js',)
 
 admin.site.register(MetaData, MetaDataAdmin)
-		
