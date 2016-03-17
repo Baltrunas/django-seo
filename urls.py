5f4ea9ae43b90a076c1fe642ef0d5e3fa2c6f5d6
Stanislav Baltrunas <stanislav@baltrunas.ru>
2016-03-17 15:44:01 +0600

    fix

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^api/json/category/list/$', views.robots, name='robots'),
+	url(r'^robots\.txt$', views.robots, name='robots'),
 ]
