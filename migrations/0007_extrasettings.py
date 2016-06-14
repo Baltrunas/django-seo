a1f75e3adcbf000cc0a35f451e1e9dd33a6a4a78
Alex <alexkarachanidi@MacBook-Pro-Alex.local>
2016-06-14 14:50:17 +0600

    fix 0007 migrate value field

--- migrations/0007_extrasettings.py
+++ migrations/0007_extrasettings.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=128, verbose_name='Name')),
                 ('key', models.CharField(max_length=128, verbose_name='Key')),
                 ('type', models.CharField(choices=[(b'text', 'Text'), (b'string', 'String'), (b'int', 'Integer'), (b'double', 'Double'), (b'file', 'File')], max_length=32, verbose_name='Type')),
-                ('value', models.FileField(blank=True, max_length=50000, null=True, upload_to=b'', verbose_name='Value')),
+                ('value', models.FileField(blank=True, max_length=20000, null=True, upload_to=b'', verbose_name='Value')),
                 ('site', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='extra_settings', to='sites.Site', verbose_name='Site')),
             ],
         ),
