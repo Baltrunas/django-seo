12a4849122f2f3bc01838243dc88a26421f67b36
Stanislav Baltrunas <stanislav@baltrunas.ru>
2022-11-21 17:43:52 +0200

    clear

--- context_processors.py
+++ context_processors.py
@@ -2,10 +2,5 @@ from .models import Data
 
 
 def seo(request):
-    try:
-        seo_data = Data.objects.get(public=True, url=request.path_info, site=request.site)
-    except:
-        seo_data = False
-    return {
-        "seo": seo_data,
-    }
+    data = Data.objects.filter(public=True, url=request.path_info, site=request.site).first()
+    return {"seo": data}
